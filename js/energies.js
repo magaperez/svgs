b4b4a034242a11da69947514fe3192119eaa78a4
magaperez <mperezduran@luc.edu>
2021-03-28 22:46:39 -0500

    Energies SVG

---
+++ js/energies.js
@@ -0,0 +1,5 @@
+const energies = document.querySelectorAll(".cls-1,.cls-11,.cls-12,.cls-13,.cls-14,.cls-15, .cls-16, .cls-17");
+
+for (let i= 0; i< energies.length; i++) {
+console.log('Letter ${i} is ${logo[i].getTotalLength()}');
+}
